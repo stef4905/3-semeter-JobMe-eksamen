94b55e24491274f7b88295ba391c33b3e3394146
Sofie Lund Mogensen <1061659@ucn.dk>
2017-11-21 18:10:21 +0100

    HTML og CDD

---
+++ JobMe-Homepage/JobMe-Homepage/Models/VMWorkHoursJobCategory.cs
@@ -0,0 +1,16 @@
+﻿
+using JobMe_Homepage.CompanyServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobMe_Homepage.Models
+{
+    public class VMWorkHoursJobCategory
+    {
+        public List<WorkHours> WorkHoursList { get; set; }
+        public List<JobCategory> JobCategoryList { get; set; }
+
+    }
+}
