e625a114d5c99659962ef26ae868add25d7df8e8
Mathias Væver Nikolajsen <1061741@ucn.dk>
2017-11-30 14:02:24 +0100

    Admin Controller

---
+++ JobMe/BusinessLogicLayer/AdminCtr.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLayer;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    public class AdminCtr : IController<Admin>
+    {
+        DbAdmin dbAdmin = new DbAdmin();
+
+        public void Create(Admin obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns the specific Admin.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Applier object</returns>
+        public Admin Get(int id)
+        {
+            Admin admin = dbAdmin.Get(id);
+            return admin;
+        }
+
+        public List<Admin> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Admin obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// returns an Admin with the given param from the login
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public Admin Login(string username, string password)
+        {
+            return dbAdmin.Login(username, password);
+        }
+    }
+}
