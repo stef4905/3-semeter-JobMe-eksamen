39fab6d4a8465e7e8ea7c600e473a88456fa8460
KasperVildner <Kaspervee@gmail.com>
2017-11-30 13:26:19 +0100

    Booking ctr

--- JobMe/BusinessLogicLayer/BookingCtr.cs
+++ JobMe/BusinessLogicLayer/BookingCtr.cs
@@ -21,5 +21,34 @@ namespace BusinessLogicLayer
         {
             return DbBooking.Create(booking);
         }
+
+        /// <summary>
+        /// Returns a booking from database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Booking get(int id)
+        {
+            return DbBooking.get(id);
+        }
+
+        /// <summary>
+        /// Returns a list of bookings
+        /// </summary>
+        /// <returns></returns>
+        public List<Booking> GetAll(int id)
+        {
+            return DbBooking.GetAll(id);
+        }
+
+        /// <summary>
+        /// Updates a booking in the database
+        /// </summary>
+        /// <param name="booking"></param>
+        /// <returns></returns>
+        public bool Update(Booking booking)
+        {
+            return DbBooking.Update(booking);
+        }
     }
 }
