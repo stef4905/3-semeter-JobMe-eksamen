dabdcd2d30fa40732b7c3ea357f96e0b438e2ec2
KasperVildner <Kaspervee@gmail.com>
2017-12-05 15:08:44 +0100

    Added som awesome comments on the algorithm in BookingCtr

--- JobMe/BusinessLogicLayer/BookingCtr.cs
+++ JobMe/BusinessLogicLayer/BookingCtr.cs
@@ -22,17 +22,27 @@ namespace BusinessLogicLayer
         {
             try
             {
+                //LastEndTime is first set to the bookings start date and time.
+                //This will later be changed to the last inserted sessions end time.
                 DateTime LastEndTime = booking.StartDateAndTime;
+                //Returnes the booking that was just inserted into the database (Only returns if the Create method works)
                 Booking bookingReturned = DbBooking.Create(booking);
+                //Finding the time difference between the start en endtime.
+                //This is used to calculate the total time available to use for interviews.
                 TimeSpan diff = booking.EndDateAndTime - bookingReturned.StartDateAndTime;
+                //availableTime is the time each applier (interview) has available to use.
+                //This is calculated by deviding the time difference from before with the total amount of interviews needed.
                 TimeSpan availableTime = new TimeSpan(diff.Ticks / bookingReturned.InterviewAmount);
 
 
-                //Calculating the endtime
+                //Calculating the first endtime by using the number of Appliers (interviews) where the total time is calculated.
+                //The endTime is then calculated by getting the original EndDateTime on the booking, where it is subtracted bu the totalEndTime, adding the availableTime.
                 Double ii = Convert.ToDouble(bookingReturned.InterviewAmount);
                 TimeSpan totalEndTime = new TimeSpan(0, Convert.ToInt16(availableTime.Minutes * ii), 0);
                 DateTime endTime = bookingReturned.EndDateAndTime - totalEndTime + availableTime;
 
+                //For each applier (interview) we now create a new session and insert it into the database.
+                //We also add the lastEndTime and EndTime here to specify the startTime and Endtime for the specific session.
                 for (int i = 1; i <= bookingReturned.InterviewAmount; i++)
                 {
 
@@ -42,9 +52,9 @@ namespace BusinessLogicLayer
                     SessionCtr.Create(session, bookingReturned);
 
 
-                    //Set the starttime on the original booking to ensure new time is sat for next session
-                    //bookingReturned.StartDateAndTime = session.StartTime.Add(availableTime);
+                    //Changine the LastEndTime to the current endtime. This will allow the next session to have a startTime, starting at de same time as the previous ended.
                     LastEndTime = endTime;
+                    //The new endTime will be the current endTime, adding the time available for each applier (interview).
                     endTime = endTime + availableTime;
                     
                 }
