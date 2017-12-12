aee8266a9a3204edb6818c1e7e6a0819d4a4857e
Christian Trab <christianmtrab@gmail.com>
2017-12-12 08:43:36 +0100

    JobPost Update & Delete Test!

--- JobMe/UnitTestJobMe/JobPostTest.cs
+++ JobMe/UnitTestJobMe/JobPostTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DataAccessLayer;
 using ModelLayer;
 using System.Collections.Generic;
+using BusinessLogicLayer;
 
 namespace UnitTestJobMe
 {
@@ -25,7 +26,7 @@ namespace UnitTestJobMe
             string iDate = "05/05/2018";
             DateTime oDate = Convert.ToDateTime(iDate);
             DateTime nowDate = oDate;
-              Meeting meeting = new Meeting
+            Meeting meeting = new Meeting
             {
                 CompanyId = 1,
                 Id = 6
@@ -38,8 +39,8 @@ namespace UnitTestJobMe
             // Act
             bool inserted = dbJobPost.Create(jobPost);
 
-           // Assert
-             Assert.IsTrue(inserted);
+            // Assert
+            Assert.IsTrue(inserted);
         }
 
         [TestMethod]
@@ -75,7 +76,7 @@ namespace UnitTestJobMe
             //Assert
             Assert.IsNotNull(list);
         }
-        
+
         [TestMethod]
         public void TestJobPostTableSize()
         {
@@ -89,6 +90,84 @@ namespace UnitTestJobMe
             //Assert
             Assert.AreEqual(14, count);
 
+        }
+
+        [TestMethod]
+        public void JobPostUpdateDBTest()
+        {
+            //Arrange
+            DbJobPost dbJobPost = new DbJobPost();
+            JobPostCtr jobPostCtr = new JobPostCtr();
+
+            Company testcompany = new Company("unittest@gmail.com", "hellopassword");
+            testcompany.Id = 1;
+            WorkHours testworkHours = new WorkHours();
+            testworkHours.Id = 1;
+            JobCategory testjobCategory = new JobCategory();
+            testjobCategory.Id = 1;
+            Meeting testmeeting = new Meeting
+            {
+                CompanyId = 1,
+                Id = 6
+
+            };
+            JobPost jobPost = new JobPost
+            {
+                Id = 5001,
+                Title = "UnitTestsss",
+                Description = "Simple Unit Testssss",
+                StartDate = new DateTime(2017, 05, 05),
+                EndDate = new DateTime(2018, 05, 05),
+                JobTitle = "Unit Test Job Titleeeeeeee",
+                workHours = testworkHours,
+                Address = "UnitTestVejURedigeret 123",
+                company = testcompany,
+                jobCategory = testjobCategory,
+                Meeting = testmeeting
+
+
+            };
+
+
+            JobPost jobPostUpdate = new JobPost
+            {
+                Id = 5001,
+                Title = "UnitTested",
+                Description = "Simple Unit Tested",
+                StartDate = new DateTime(2017, 05, 05),
+                EndDate = new DateTime(2018, 05, 05),
+                JobTitle = "Unit Test Job Title",
+                workHours = testworkHours,
+                Address = "UnitTestVej 321",
+                company = testcompany,
+                jobCategory = testjobCategory,
+                Meeting = testmeeting
+
+
+            };
+            //Act
+            jobPostCtr.Create(jobPost);
+            jobPostCtr.Update(jobPostUpdate);
+
+            //Assert
+            Assert.AreNotEqual(jobPost, jobPostUpdate);
+
+
+
+        }
+        [TestMethod]
+        public void DeleteJobPost()
+        {
+            //Arrange
+            JobPostCtr jobPostCtr = new JobPostCtr();
+
+            //Act
+            jobPostCtr.Delete(1050);
+            JobPost deletedJobPost = jobPostCtr.Get(1050);
+
+
+            //Assert
+            Assert.IsNull(deletedJobPost.Id);
         }
     }
 }
